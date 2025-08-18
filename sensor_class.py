4dac518b8bfc0de221c3afda2ece7bca1694fa84
Krisha Raj <krisha.raj@education.nsw.gov.au>
2025-08-19 09:20:36 +1000

    sensor_class.py

--- sensor_class.py
+++ sensor_class.py
@@ -1,10 +1,11 @@
 from machine import Pin, time_pulse_us
 
+#Parent sensor Class
 class Sensor:
     def __init__(self, sensorPin):
         self.sensorPin = sensorPin
         self.objectDetected = False
-        
+    #Set up sensor
     def setUp():
         self.sensorPin = Pin(sensorPin, Pin.OUT)   
 
@@ -12,17 +13,17 @@ class Sensor:
         pass
 
 
-
+#Chidl class of Sensor Class
 class UltrasonicSensor(Sensor):
     def __init__(self, sensorPin, echoPin):
         super().__init__(sensorPin)
         self.detectDistance = 60
         self.echoPin = echoPin
-    
+    #Set up the ultrasonic sensor
     def setUp():
         super().setUp()
         self.echoPin = Pin(echoPin, Pin.IN)
-
+    #Checks if there is an object detected and return True or False
     def checkData(self):    
         self.sensorPin.off()
         self.sensorPin.sleep_us(2)
@@ -38,19 +39,20 @@ class UltrasonicSensor(Sensor):
                 self.objectDetected = True        
         else:
             self.objectDetected = False
-        
+    #Sends data to main.py
     def sendData(self):
         checkData()
         return self.objectDetected
 
-
+#Child class of Sensor Class
 class PIRsensor(Sensor):
     def __init__(self, sensorPin):
         super().__init__(sensorPin)
     
+    #Set up PIR Sensor
     def setUp(self):
         self.sensorPin = Pin(sensorPin, Pin.In)
-    
+    #Check is motion detected, and return True or False
     def checkData(self):
         if self.sensorPin.value():
             self.objectDetected = True
@@ -59,6 +61,8 @@ class PIRsensor(Sensor):
         
         return self.objectDetected
     
+    #Send data to main.py
     def sendData(self):
         checkData()
         return self.objectDetected
+
