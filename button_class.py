f92af1f96afc77f470e9658dcdb23c105a357806
Krisha Raj <krisha.raj@education.nsw.gov.au>
2025-08-19 09:18:37 +1000

    button_class.py

--- button_class.py
+++ button_class.py
@@ -1,15 +1,17 @@
 from machine import Pin
 import time
 
+#Button Class
 class Button():
     def __init__(self, button_pin):
         self.button_pin = button_pin
         self.button_pressed = False
         self.on = False
-    
+    #Set up button
     def setUp():
         self.button_pin = Pin(button_pin, Pin.IN, Pin.PULL_UP)
-
+    
+    #Check is button pressed, and return True or False
     def button_pressed():
         first = button.value()
         time.sleep(0.01)
@@ -18,8 +20,10 @@ class Button():
             self.button_pressed = True
         else:
             self.button_pressed = False
+        #Returns True or False
         return self.button_pressed
     
             
   
         
+
